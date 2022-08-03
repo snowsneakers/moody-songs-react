dc1e2cfc3e7bbcbbaba6178c93857322bc6e61d2
Patrick Snowden <patrickbsnowden@outlook.com>
2022-08-03 11:16:12 -0700

    changed styles

--- src/components/Lyrics.js
+++ src/components/Lyrics.js
@@ -2,7 +2,7 @@ const Lyrics = ({ song }) => {
      const newLyrics = song.lyrics.split("<br>").join(" \n");
      return (
           <section className="min-w-[316px] max-h-screen cursor-pointer whitespace-pre-wrap sticky top-0 p-2 flex items-center justify-center mb-10">
-               <div className="lyrics max-h-[400px] min-w-[320px] overflow-scroll shadow-md rounded p-2">
+               <div className="lyrics max-h-[400px] min-w-[320px] overflow-scroll rounded p-2">
                     <h3 className="title text-2xl font-semibold text-center">
                          Lyrics
                     </h3>
